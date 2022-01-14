1f2a35233b09b1b787516b55c6f62f02eb4d2a3b
PAStephenson <82534032+PAStephenson@users.noreply.github.com>
2022-01-14 22:56:51 +0000

    Added ability to perform the four basic operations

--- calculator.js
+++ calculator.js
@@ -34,3 +34,84 @@ const btnMinus = document.querySelector("#btnMinus");
 const btnTimes = document.querySelector("#btnTimes");
 const btnDivide = document.querySelector("#btnDivide");
 const btnEquals = document.querySelector("#btnEquals");
+
+let numberInput = [];
+let firstNumber;
+let secondNumber;
+let operation;
+
+btn0.addEventListener("click", () => {numberInput.push(0)});
+btn1.addEventListener("click", () => {numberInput.push(1)});
+btn2.addEventListener("click", () => {numberInput.push(2)});
+btn3.addEventListener("click", () => {numberInput.push(3)});
+btn4.addEventListener("click", () => {numberInput.push(4)});
+btn5.addEventListener("click", () => {numberInput.push(5)});
+btn6.addEventListener("click", () => {numberInput.push(6)});
+btn7.addEventListener("click", () => {numberInput.push(7)});
+btn8.addEventListener("click", () => {numberInput.push(8)});
+btn9.addEventListener("click", () => {numberInput.push(9)});
+
+btnPlus.addEventListener("click", () => {assignOperation("+")});
+btnMinus.addEventListener("click", () => {assignOperation("-")});
+btnTimes.addEventListener("click", () => {assignOperation("*")});
+btnDivide.addEventListener("click", () => {assignOperation("/")});
+btnEquals.addEventListener("click", () => {performCalc()});
+
+function assignOperation(op) {
+	assignNumber();
+	operation = op;
+}
+
+function assignNumber() {
+	if (isNaN(firstNumber)) {
+		firstNumber = parseInt(numberInput.join(""));
+		numberInput = [];
+		return;
+	} else if (isNaN(secondNumber)) {
+		secondNumber = parseInt(numberInput.join(""));
+		numberInput = [];
+		return;
+	}
+}
+
+function performCalc() {
+	assignNumber();
+
+	switch (operation) {
+		case "+":
+			performAddition();
+			break;
+		case "-":
+			performSubtraction();
+			break;
+		case "*":
+			performMultiplication();
+			break;
+		case "/":
+			performDivision();
+	}
+}
+
+function performAddition() {
+	firstNumber += secondNumber;
+	secondNumber = null;
+	console.log(firstNumber);
+}
+
+function performSubtraction() {
+	firstNumber -= secondNumber;
+	secondNumber = null;
+	console.log(firstNumber);
+}
+
+function performMultiplication() {
+	firstNumber *= secondNumber;
+	secondNumber = null;
+	console.log(firstNumber);
+}
+
+function performDivision() {
+	firstNumber /= secondNumber;
+	secondNumber = null;
+	console.log(firstNumber);
+}
