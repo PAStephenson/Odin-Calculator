fd028db93b06a9dfb2ef0cee7137b6be2e22a4b2
PAStephenson <82534032+PAStephenson@users.noreply.github.com>
2022-01-15 21:36:39 +0000

    Added displaying of calculation and result on screen

--- calculator.js
+++ calculator.js
@@ -17,6 +17,9 @@
 //
 // if no, do what a real calculator does
 
+// Calculator display
+const display = document.querySelector("#display");
+
 // Number buttons
 const btn0 = document.querySelector("#btn0");
 const btn1 = document.querySelector("#btn1");
@@ -40,22 +43,40 @@ let firstNumber;
 let secondNumber;
 let operation;
 
-btn0.addEventListener("click", () => {numberInput.push(0)});
-btn1.addEventListener("click", () => {numberInput.push(1)});
-btn2.addEventListener("click", () => {numberInput.push(2)});
-btn3.addEventListener("click", () => {numberInput.push(3)});
-btn4.addEventListener("click", () => {numberInput.push(4)});
-btn5.addEventListener("click", () => {numberInput.push(5)});
-btn6.addEventListener("click", () => {numberInput.push(6)});
-btn7.addEventListener("click", () => {numberInput.push(7)});
-btn8.addEventListener("click", () => {numberInput.push(8)});
-btn9.addEventListener("click", () => {numberInput.push(9)});
+btn0.addEventListener("click", () => {updateInput(0)});
+btn1.addEventListener("click", () => {updateInput(1)});
+btn2.addEventListener("click", () => {updateInput(2)});
+btn3.addEventListener("click", () => {updateInput(3)});
+btn4.addEventListener("click", () => {updateInput(4)});
+btn5.addEventListener("click", () => {updateInput(5)});
+btn6.addEventListener("click", () => {updateInput(6)});
+btn7.addEventListener("click", () => {updateInput(7)});
+btn8.addEventListener("click", () => {updateInput(8)});
+btn9.addEventListener("click", () => {updateInput(9)});
 
-btnPlus.addEventListener("click", () => {assignOperation("+")});
-btnMinus.addEventListener("click", () => {assignOperation("-")});
-btnTimes.addEventListener("click", () => {assignOperation("*")});
-btnDivide.addEventListener("click", () => {assignOperation("/")});
+btnPlus.addEventListener("click", () => {updateInput(" + ")});
+btnMinus.addEventListener("click", () => {updateInput(" - ")});
+btnTimes.addEventListener("click", () => {updateInput(" * ")});
+btnDivide.addEventListener("click", () => {updateInput(" / ")});
 btnEquals.addEventListener("click", () => {performCalc()});
+
+function updateInput(input) {
+	if (isNaN(input)) {
+		assignOperation(input);
+		updateDisplay(input);
+	} else {
+		numberInput.push(input);
+		updateDisplay(input);
+	}
+}
+
+function updateDisplay(input) {
+	display.append(input);
+}
+
+function displayResult() {
+	display.textContent = firstNumber;
+}
 
 function assignOperation(op) {
 	assignNumber();
@@ -78,40 +99,41 @@ function performCalc() {
 	assignNumber();
 
 	switch (operation) {
-		case "+":
+		case " + ":
 			performAddition();
 			break;
-		case "-":
+		case " - ":
 			performSubtraction();
 			break;
-		case "*":
+		case " * ":
 			performMultiplication();
 			break;
-		case "/":
+		case " / ":
 			performDivision();
+			break;
 	}
 }
 
 function performAddition() {
 	firstNumber += secondNumber;
 	secondNumber = null;
-	console.log(firstNumber);
+	displayResult();
 }
 
 function performSubtraction() {
 	firstNumber -= secondNumber;
 	secondNumber = null;
-	console.log(firstNumber);
+	displayResult();
 }
 
 function performMultiplication() {
 	firstNumber *= secondNumber;
 	secondNumber = null;
-	console.log(firstNumber);
+	displayResult();
 }
 
 function performDivision() {
 	firstNumber /= secondNumber;
 	secondNumber = null;
-	console.log(firstNumber);
+	displayResult();
 }
